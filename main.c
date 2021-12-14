bcf823726744f60b15a2d5316c58cb47a479790d
Merryl Zoure <merryl.zoure@edu.devinci.fr>
2021-12-14 21:21:11 +0100

    guessing game

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 int main(){
-	for (int i=1; i<=10; i++){
-		printf("%d\n", i);
+	srand(time(NULL));
+	printf("I have in mind a number in between 1 and 100, can you find it?\n");
+	int n = rand() % 100;
+	int a = 0;
+	scanf("%d",&a);
+	while(n != a){
+		if(a > 0 && a <= 100){
+			if(a < n){
+				printf("The number to guess is higher\n");
+				scanf("%d",&a);
+			}
+			else if(a > n){
+				printf("The number to guess is lower\n");
+				scanf("%d",&a);
+			}
+		}
+		else{
+			printf("Invalid argument");
+			return 1;
+		}
 	}
+	printf("You just found the number, it was indeed %d\n", n);
 	return 0;
 }
